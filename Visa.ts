417b75cbfd58dbc7827a00f99e30a33c18e9ea9b
Ayesha-Ikram <160381040+Ayesha-Ikram@users.noreply.github.com>
2024-03-19 22:17:34 +0500

    Add files via upload

---
+++ Visa.ts
@@ -0,0 +1,33 @@
+
+import inquirer from "inquirer";
+const Confirmation = await inquirer.prompt([
+    {
+        type:"confirm",
+        name:"passport",
+        message:"do you have passport?"
+    },
+    {
+        type:"confirm",
+        name:"Vissa",
+        message:"do you have Vissa?"
+    },
+])
+
+if(Confirmation.passport )
+{
+   if(Confirmation.Vissa )
+   {
+               console.log("Congratulations! you can visit abroad :)");
+               
+   }
+   else
+   {
+    console.log("Sorry you do not have Vissa.");
+    
+   }
+}
+else
+{
+    console.log("Sorry you do not have Passport.");
+    
+}
